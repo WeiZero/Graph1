b0b156b150916082bf18685eb99e9ddbe9e37aff
WeiZero <cd232310@gmail.com>
2018-11-16 23:50:13 +0800

    Update Blur

--- Robot-RX78/Robot/Shader/Blur.fs
+++ Robot-RX78/Robot/Shader/Blur.fs
@@ -1,43 +1,62 @@
-#version 410 core   
-
+#version 410 core     
+ 
 out vec4 FragColor;
 
 in vec2 TexCoords;
 
-uniform sampler2D screenTexture;  
-
-void main(void) {
-	//this will be our RGBA sum
-	vec4 sum = vec4(0.0);
-	
-	//our original texcoord for this fragment
-	vec2 tc = TexCoords;
-	
-	//the amount to blur, i.e. how far off center to sample from 
-	//1.0 -> blur by one pixel
-	//2.0 -> blur by two pixels, etc.
-	float blur = 2.0; 
-    
-	//the direction of our blur
-	//(1.0, 0.0) -> x-axis blur
-	//(0.0, 1.0) -> y-axis blur
-	float hstep = 1.0;
-	float vstep = 0.0;
-    
-	//apply blurring, using a 9-tap filter with predefined gaussian weights
-    
-	sum += texture2D(screenTexture, vec2(tc.x - 4.0*blur*hstep, tc.y - 4.0*blur*vstep)) * 0.0162162162;
-	sum += texture2D(screenTexture, vec2(tc.x - 3.0*blur*hstep, tc.y - 3.0*blur*vstep)) * 0.0540540541;
-	sum += texture2D(screenTexture, vec2(tc.x - 2.0*blur*hstep, tc.y - 2.0*blur*vstep)) * 0.1216216216;
-	sum += texture2D(screenTexture, vec2(tc.x - 1.0*blur*hstep, tc.y - 1.0*blur*vstep)) * 0.1945945946;
-	
-	sum += texture2D(screenTexture, vec2(tc.x, tc.y)) * 0.2270270270;
-	
-	sum += texture2D(screenTexture, vec2(tc.x + 1.0*blur*hstep, tc.y + 1.0*blur*vstep)) * 0.1945945946;
-	sum += texture2D(screenTexture, vec2(tc.x + 2.0*blur*hstep, tc.y + 2.0*blur*vstep)) * 0.1216216216;
-	sum += texture2D(screenTexture, vec2(tc.x + 3.0*blur*hstep, tc.y + 3.0*blur*vstep)) * 0.0540540541;
-	sum += texture2D(screenTexture, vec2(tc.x + 4.0*blur*hstep, tc.y + 4.0*blur*vstep)) * 0.0162162162;
-
-	//discard alpha for our simple demo, multiply by vertex color and return
-	FragColor = vec4(sum.rgb,1.0);
+uniform sampler2D screenTexture;      
+ 
+const vec2 sketchSize = vec2(512., 512.);
+const float barrelPower = 0.4; 
+const int num_iter = 10;
+const float reci_num_iter_f = 1.0 / float(num_iter); // 用于迭代时归一化
+ 
+vec2 barrelDistortion(vec2 coord, float amt) // 随着迭代次数增加，像素偏移更大；距离图像中心越远，像素偏移更大。
+{
+    vec2 cc = coord - 0.5;
+    float dist = dot(cc, cc);
+    return coord + cc * dist * amt; 
+}
+ 
+float sat( float t )
+{
+    return clamp( t, 0.0, 1.0 );
+}
+ 
+float linterp( float t ) {
+    return sat( 1.0 - abs( 2.0*t - 1.0 ) );
+}
+ 
+float remap( float t, float a, float b ) 
+{
+    return sat( (t - a) / (b - a) );
+}
+ 
+vec3 spectrum_offset( float t ) 
+{
+    vec3 ret;
+    float lo = step(t,0.5);
+    float hi = 1.0-lo;
+    float w = linterp( remap( t, 1.0/6.0, 5.0/6.0 ) );
+    ret = vec3(lo,1.0,hi) * vec3(1.0-w, w, 1.0-w);
+ 
+    return pow( ret, vec3(1.0/2.2) );
+}
+ 
+void main()
+{   
+    vec2 uv = TexCoords;
+ 
+    vec3 sumcol = vec3(0.0);
+    vec3 sumw = vec3(0.0);  
+    for ( int i=0; i<num_iter;++i )
+    {
+        float t = float(i) * reci_num_iter_f;
+        vec3 w = spectrum_offset( t );   // RGB三通道分别的权重
+        sumw += w;                       // 用于之后归一化权重
+		uv = barrelDistortion(uv, barrelPower*t );
+        sumcol += w * texture2D( screenTexture, uv).rgb; 
+    } 
+        
+    FragColor = vec4(sumcol.rgb / sumw, 1.0);
 }
